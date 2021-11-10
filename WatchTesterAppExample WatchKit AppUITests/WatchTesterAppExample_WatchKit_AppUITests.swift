1054eca3424650265103532cfca84511a1797540
Zhanna Moskaliuk <octoberrock11@gmail.com>
2021-11-10 17:42:41 +0200

    Cleaned useless setup

--- WatchTesterAppExample WatchKit AppUITests/WatchTesterAppExample_WatchKit_AppUITests.swift
+++ WatchTesterAppExample WatchKit AppUITests/WatchTesterAppExample_WatchKit_AppUITests.swift
@@ -12,32 +12,19 @@ class WatchTesterAppExample_WatchKit_AppUITests: XCTestCase {
     
     var watchTester: WatchTester!
     
-    let timeout: TimeInterval = 60 * 60 * 24
-    
-    private var expectation: XCTestExpectation?
-    
     private let app = XCUIApplication()
     
     override func setUp() {
         super.setUp()
         
-        expectation = XCTestExpectation(description: "All tests are finished.")
-        
-        continueAfterFailure = false
+//        continueAfterFailure = false
         
         // init watch tester
         // then start websocket connection
         watchTester = WatchTester(app: app)
         watchTester.connect()
         
-        watchTester.shutdownCompletion = { [weak self] in
-            guard let self = self else { return }
-            self.expectation?.fulfill()
-        }
-        
-        // wait untill connection is stablished
 //        app.launch()
-        wait(for: [expectation!], timeout: timeout)
     }
     
     func testLaunch() {
