8ac8971c4270e6422b6215d838dbd6b22bc70fdd
Zhanna Moskaliuk <octoberrock11@gmail.com>
2022-03-07 17:08:06 +0100

    Add complication to the test app

--- WatchTesterAppExample WatchKit Extension/ComplicationController.swift
+++ WatchTesterAppExample WatchKit Extension/ComplicationController.swift
@@ -7,25 +7,27 @@
 
 import ClockKit
 
-
 class ComplicationController: NSObject, CLKComplicationDataSource {
     
     // MARK: - Complication Configuration
-
+    
     func getComplicationDescriptors(handler: @escaping ([CLKComplicationDescriptor]) -> Void) {
-        let descriptors = [
-            CLKComplicationDescriptor(identifier: "complication", displayName: "WatchTesterAppExample", supportedFamilies: CLKComplicationFamily.allCases)
-            // Multiple complication support can be added here with more descriptors
-        ]
+        
+        let bellyrubsIdentifier = "testComplication"
+        let supportedFamilies = [CLKComplicationFamily.graphicCorner, .graphicCircular, .circularSmall]
+        let bellyRubsDescriptor = CLKComplicationDescriptor(
+            identifier: bellyrubsIdentifier,
+            displayName: "Bellyrubs",
+            supportedFamilies: supportedFamilies)
         
         // Call the handler with the currently supported complication descriptors
-        handler(descriptors)
+        handler([bellyRubsDescriptor])
     }
     
     func handleSharedComplicationDescriptors(_ complicationDescriptors: [CLKComplicationDescriptor]) {
         // Do any necessary work to support these newly shared complication descriptors
     }
-
+    
     // MARK: - Timeline Configuration
     
     func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
@@ -37,23 +39,44 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         // Call the handler with your desired behavior when the device is locked
         handler(.showOnLockScreen)
     }
-
+    
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-        // Call the handler with the current timeline entry
-        handler(nil)
+        if let template = getComplicationTemplate(for: complication, using: Date()) {
+            let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+            handler(entry)
+        } else {
+            handler(nil)
+        }
     }
     
     func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
         // Call the handler with the timeline entries after the given date
         handler(nil)
     }
-
+    
     // MARK: - Sample Templates
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        let template = getComplicationTemplate(for: complication, using: Date())
+        if let t = template {
+            handler(t)
+        } else {
+            handler(nil)
+        }
+    }
+    
+    private func getComplicationTemplate(for complication: CLKComplication, using date: Date) -> CLKComplicationTemplate? {
+        switch complication.family {
+        case .graphicCorner:
+            return CLKComplicationTemplateGraphicCornerCircularImage(imageProvider: CLKFullColorImageProvider(fullColorImage: UIImage(named: "Complication/Graphic Corner")!))
+        case .graphicCircular:
+            return CLKComplicationTemplateGraphicCircularImage(imageProvider: CLKFullColorImageProvider(fullColorImage: UIImage(named: "Complication/Graphic Circular")!))
+        case .circularSmall:
+            return CLKComplicationTemplateCircularSmallSimpleImage(imageProvider: CLKImageProvider(onePieceImage: UIImage(named: "Complication/Circular")!))
+        default:
+            return nil
+        }
     }
 }
