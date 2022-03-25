47d7f86f4f08ffa30f50982d997984600edc3d5d
Zhanna Moskaliuk <octoberrock11@gmail.com>
2022-03-25 14:16:24 +0100

    Add long press gesture recogniser for testing purposes

--- WatchTesterAppExample WatchKit Extension/EventInterfaceController.swift
+++ WatchTesterAppExample WatchKit Extension/EventInterfaceController.swift
@@ -14,6 +14,12 @@ class EventInterfaceController: WKInterfaceController {
     @IBOutlet weak var eventImage: WKInterfaceImage!
     @IBOutlet weak var eventCategory: WKInterfaceLabel!
     @IBOutlet weak var eventLabel: WKInterfaceLabel!
+    @IBOutlet weak var longPressGesture: WKLongPressGestureRecognizer!
+    var pushWristLocationScreen = false
+    
+    @IBAction func longPress(_ sender: Any) {
+        goToWristLocationScreen()
+    }
     
     var event: Event? {
         didSet {
@@ -23,18 +29,29 @@ class EventInterfaceController: WKInterfaceController {
             eventImage.setImage(event.category?.eventImage)
             eventImage.setTintColor(event.category?.eventColor)
             eventCategory.setTextColor(event.category?.eventColor)
-
+            
             eventLabel.setAccessibilityIdentifier("detailsScreenEventLabel")
             eventCategory.setAccessibilityIdentifier("detailsScreenEventCategory")
             eventImage.setAccessibilityIdentifier("detailsScreenEventImage")
+            
         }
     }
-
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-
+        
+        longPressGesture.minimumPressDuration = 2
+        
         if let event = context as? Event {
             self.event = event
         }
+    }
+    
+    private func goToWristLocationScreen() {
+        guard !pushWristLocationScreen else {
+            return
+        }
+        self.pushController(withName: "WristLocationInterfaceController", context: nil)
+        pushWristLocationScreen = true
     }
 }
